196b5e04ca6fe440efde3520a2e8d5b6ff81ba55
userkim12 <potomq444@naver.com>
2023-06-07 15:46:53 +0900

    add lunch random fetcher

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+import lunch
+
 # client = MongoClient('')
 # db = client.dbsparta
 app = Flask(__name__)
@@ -7,6 +9,13 @@ app = Flask(__name__)
 @app.route('/')
 def show_home():
    return render_template('index.html')
+
+@app.route('/lunch')
+def get_lunch():
+   food_name, img = lunch.get_random_lunch()
+
+   
+   
 
 if __name__ == '__main__':
    app.run()
