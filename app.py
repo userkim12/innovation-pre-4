161530b0315f5fbae21ac8734f987b47a3856f38
DESKTOP-6LNCCIA\기미년 <minhyun17@naver.com>
2023-06-07 18:04:46 +0900

    운세데이터가져오기

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import lunch
 import advice
 import json
+import random
 
 # client = MongoClient('')
 # db = client.dbsparta
@@ -37,7 +38,22 @@ def get_advice():
     result = make_response(json.dumps(data, ensure_ascii=False, indent=4))
     return result
 
+def read_luck():
+    f = open("today_luck.txt", 'r',encoding='utf-8')
+    lines = f.readlines() 
+    return lines
 
+luck = read_luck()
+
+@app.route("/luck", methods=["GET"])
+def get_luck():
+    random_luck = random.sample(luck,3)
+    luck_data = {
+        'card1': random_luck[0].strip(),
+        'card2': random_luck[1].strip(),
+        'card3': random_luck[2].strip()
+    }
+    return jsonify(luck_data)
 
 if __name__ == '__main__':
    app.run()
